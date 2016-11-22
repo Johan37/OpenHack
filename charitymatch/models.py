761b97fa8113f11efbec85eea687e88b6d623e64
johan <johan.philipsson@hotmail.com>
2016-11-22 19:33:19 +0100

    Add first draft of model

--- charitymatch/models.py
+++ charitymatch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=200)
+    categorys = models.ManyToManyField(Category)
+    
+    def __str__(self):
+        return self.name
+
--- openhack16/settings.py
+++ openhack16/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'charitymatch.apps.CharitymatchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
