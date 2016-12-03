3900b80e3922e272524cd523194b6a62f7ca64a2
jakob <jakob.willforss@hotmail.com>
2016-12-03 17:05:50 +0100

    Some unstructured search query code

--- charitymatch/searchQuery.py
+++ charitymatch/searchQuery.py
@@ -7,6 +7,7 @@ from django.db import models
 from .models import Category, SubCategory, Organisation, Question
 
 def getSearchResults(request):
+
     #translate the JSON body to Python
     searchParameters = []
     if request.is_ajax():
@@ -19,12 +20,58 @@ def getSearchResults(request):
     else:
         return JsonResponse(status=500, data={'status':'false','message':"Internal error: The request is not ajax"})
 
-    #Print the users search parameters. This is compared with organisation parameters
-    print(searchParameters)
+    # Print the users search parameters. This is compared with organisation parameters
+    print("Search params {}".format(searchParameters['answers']))
 
-    #Get the data from the database
-    #organisationList = list(Organisation.objects.filter(categories__contains=1).values("name", "categories", "countries"))
-    #print(organisationList)
+    search_param_dict = searchParameters['answers']
+
+
+    organisationList = list(Organisation.objects.all())
+
+
+    picked_subjects = search_param_dict['subject']
+    picked_subjects.append('Homless')
+
+    print('Picked subjects: {}'.format(picked_subjects))
+
+    matching_entries = []
+    for subject in picked_subjects:
+
+        matching_entries = [org for org in organisationList if subject in list(org.categories.all())]
+
+
+
+    print('Matching entires {}'.format(matching_entries))
+
+
+
+    # for key in search_param_dict:
+    #     print(search_param_dict[key])
+    #
+    #     picked_values_list = search_param_dict[key]
+
+
+
+
+
+
+
+
+
+
+    # Get the data from the database
+
+    # organisationList = list(Organisation.objects.values("name", "categories", "countries"))
+    print(organisationList)
+
+    organisationCategories = list(Organisation.objects.values("categories"))
+
+    for org in organisationList:
+
+        categories = list(org.categories.all())
+
+        for sub in categories:
+            print("Name: {}".format(sub.name))
 
 
     #Calculate score per organisation
